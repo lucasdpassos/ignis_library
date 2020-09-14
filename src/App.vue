d7ddbd5f24bfd3b63d77dd23146c086606f75a9f
Lucas Passos <lucas.depassos@yahoo.com.br>
2020-09-13 21:01:06 -0300

    categories update

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,11 @@
         <v-img src="./assets/logo.png" style="max-width:300px; margin-top:-1%">
 
         </v-img>
-
+        <v-text-field class='display-0 white--text font-weight-thin subtitle-1' style="margin-left: 5%; margin-top:5%">Tecnologia à favor da sua expansão de consciência</v-text-field>
     </v-app-bar>
 
     <v-main>
+
         <Body />
     </v-main>
 
--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,89 +1,112 @@
 <template>
-    <v-container>
-    <v-row dense>       
-        <v-col
-          v-for="(category, i) in categories"
-          :key="i"
-          cols="3"
-        >
-         <v-hover v-slot:default="{ hover }">
-          <v-card            
-            dark
-            style="max-width:300px;"
-            
-          >
-          <v-img 
-          :src="category.Bannersrc"
-          >
-          <v-expand-transition>
-          <div
-            v-if="hover"
-            class="d-flex transition-fast-in-fast-out amber darken-2 v-card--reveal display-1 white--text"
-            style="height: 100%;"
-             v-text="category.name"
-          >
-            
-          </div>
-        </v-expand-transition>
-          
-                        
+<v-container>
+    <v-row dense>
+        <v-col v-for="(category, i) in categories" :key="i" cols="3">
+            <v-hover v-slot:default="{ hover }">
+                <v-card dark max-width="200" class="mx-auto">
+                    <v-img :src="category.Bannersrc">
+                        <v-expand-transition>
+                            <div v-if="hover" class="d-flex transition-fast-in-fast-out amber darken-2 v-card--reveal display-0 white--text" style="height: 100%;" v-text="category.name">
 
-    
-            </v-img>
-          </v-card>
-         </v-hover>
-          
+                            </div>
+                        </v-expand-transition>
+
+                    </v-img>
+                </v-card>
+            </v-hover>
+
         </v-col>
-      </v-row>
-      <User />
-    </v-container> 
-    
+    </v-row>
+    <User />
+</v-container>
 </template>
-
-
 
 <script>
 export default {
     data: () => {
-         return {
-      current: {},
-      index: 0,      
-      categories: [
-        {
-               
-          Bannersrc: require('../assets/cat/wicca.jpg'),
-          name: "Wicca/Pagan"
-        },       
-                  
-        {
-               
-          Bannersrc: require('../assets/cat/candumb.jpg'),
-          name: "Candomblé/Umbanda"
-        },       
-        {
-               
-          Bannersrc: require('../assets/cat/herm.jpg'),
-          name: "Hermetismo"
-        },       
-                  
-      ],
-     
-    }    
+        return {
+            current: {},
+            index: 0,
+            categories: [{
+
+                    Bannersrc: require('../assets/cat/wicca.jpg'),
+                    name: "Wicca/Pagan"
+                },
+
+                {
+
+                    Bannersrc: require('../assets/cat/candumb.jpg'),
+                    name: "Candomblé/Umbanda"
+                },
+                {
+
+                    Bannersrc: require('../assets/cat/herm.jpg'),
+                    name: "Hermetismo"
+                },
+                {
+
+                    Bannersrc: require('../assets/cat/alq.jpg'),
+                    name: "Alquimia"
+                },
+                {
+
+                    Bannersrc: require('../assets/cat/anc.jpg'),
+                    name: "Conhecimento Ancestral"
+                },
+                {
+
+                    Bannersrc: require('../assets/cat/asa.jpg'),
+                    name: "Asatrú / Odinismo"
+                },
+                {
+
+                    Bannersrc: require('../assets/cat/ast.jpg'),
+                    name: "Viagem Astral"
+                },
+                {
+
+                    Bannersrc: require('../assets/cat/egi.jpg'),
+                    name: "Magia Egípcia"
+                },
+                {
+
+                    Bannersrc: require('../assets/cat/elem.jpg'),
+                    name: "Magia Elemental"
+                },
+                {
+
+                    Bannersrc: require('../assets/cat/evo.jpg'),
+                    name: "Evocação"
+                },
+                {
+
+                    Bannersrc: require('../assets/cat/taro.jpg'),
+                    name: "Tarot"
+                },
+                {
+
+                    Bannersrc: require('../assets/cat/astro.jpg'),
+                    name: "Astrologia"
+                },
+
+            ],
+
+        }
     },
     components: {
-      //
-  },
-    
+        //
+    },
+
 }
 </script>
+
 <style>
 .v-card--reveal {
-  align-items: center;
-  bottom: 0;
-  justify-content: center;
-  opacity: .5;
-  position: absolute;
-  width: 100%;
+    align-items: center;
+    bottom: 0;
+    justify-content: center;
+    opacity: .5;
+    position: absolute;
+    width: 100%;
 }
 </style>
-
